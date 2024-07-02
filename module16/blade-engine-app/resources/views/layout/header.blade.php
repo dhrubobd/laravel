766466bd2bc7f08863aeaff68fe86b8761c1b127
Luke Kalyan Brata Sarker <dhrubo.luke@gmail.com>
2024-07-02 11:42:43 +0600

    blade engine practice done.

---
+++ module16/blade-engine-app/resources/views/layout/header.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Page title</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="{{asset('css/bootstrap/bootstrap.min.css')}}">
+    <link rel="icon" type="image/png" sizes="32x32" href="shuffle-for-bootstrap.png">
+</head>
+<body>
+    <div class="container-fluid">
