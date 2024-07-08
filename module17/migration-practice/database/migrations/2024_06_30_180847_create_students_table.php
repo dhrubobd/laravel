7014e5f30fb42712f5f9537b92aa30549fc73689
Luke Kalyan Brata Sarker <dhrubo.luke@gmail.com>
2024-07-08 21:51:56 +0600

    Laravel Database Migration Practice Done.

---
+++ module17/migration-practice/database/migrations/2024_06_30_180847_create_students_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',100)->index();
+            $table->integer('roll_no');
+            $table->string('class',10)->index();
+            $table->enum('section',['A','B','C'])->default('A');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
